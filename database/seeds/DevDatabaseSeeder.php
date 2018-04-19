67cb9393c0076ddf91232df65d0e6b2dd5086b32
James Gifford <jg.webdeveloper@gmail.com>
2018-04-19 14:04:11 -0700

    Setup testing with database listener and seeders.

---
+++ database/seeds/DevDatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DevDatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database with development data.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Dev seeders to run
+        // $this->call(DevSeeder::class);
+    }
+}
