b82f20cdee7142a8c75124a102ca286eae9ea430
James Gifford <jg.webdeveloper@gmail.com>
2018-04-24 02:18:37 -0700

    Add posts table migration.

---
+++ database/migrations/2018_04_19_225014_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->string('subtitle')->default('');
+            $table->text('content');
+            $table->boolean('is_featured')->default(false);
+            $table->string('category');
+            $table->string('image');
+            $table->string('link');
+            $table->timestamp('published_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
