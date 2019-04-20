438d114b8bccf5abbdef12c9967c6c38b798e566
James Gifford <jgifford@discogsinc.com>
2019-04-20 14:30:29 -0700

    Update resume links on homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,7 +8,7 @@
             <h1 class="heading mb-4">Hello! My name is James Gifford and I enjoy working on web projects of all shapes and sizes.</h1>
             <p class="lead text-muted">Whether by writing clean code, implementing user interfaces, or testing new features I can assist with all aspects of web development to create solid web experiences. Please let me know if you need help with your next project.</p>
             <p class="my-5">
-                <a href="/work" class="btn btn-lg btn-outline-primary mx-2">Check out my work</a>
+                <a href="/resume" class="btn btn-lg btn-outline-primary mx-2">Check out my work</a>
                 <a href="/contact" class="btn btn-lg btn-primary mx-2">Let's get in touch</a>
             </p>
         </div>
@@ -69,7 +69,7 @@
                     <img class="img-fluid mx-auto" src="/img/{{ $post->image }}" />
                 </div>
                 <div class="col-md-10 mb-3">
-                    <h2><a href="/work/{{ $post->slug }}">{{ $post->title }}</a></h2>
+                    <h2><a href="/resume/#experience">{{ $post->title }}</a></h2>
                     <p class="lead">{!! $post->first_paragraph !!}</p>
                 </div>
             </div>
